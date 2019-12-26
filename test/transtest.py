32793334f7c3f202c335a1318459b39bd53699ea
Fanrende <zxcj04@gmail.com>
2019-12-26 17:22:17 +0800

    upload

--- test/transtest.py
+++ test/transtest.py
@@ -1,20 +1,8 @@
 
 with open("test/tmp/test.txt", "r") as fp:
 
-    ori = fp.readlines()
+    ori = fp.read()
+
+ori = ori.split("\n")
 
 print(ori)
-
-first = "\""
-replacement = "\", \""
-last = "\""
-
-ori = replacement.join(ori)
-
-ori = first + ori.replace("\n", "") + last
-
-ori = ori.replace("\"\", ", "")
-
-with open("test/tmp/new.txt", "w") as fp:
-
-    fp.write(ori)
