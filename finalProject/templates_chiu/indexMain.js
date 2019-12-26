c86f969421af74b655788e3ada9e66098164963b
agimz0330 <agimz0330@gmail.com>
2019-12-26 14:53:29 +0800

    index+register

--- finalProject/templates_chiu/indexMain.js
+++ finalProject/templates_chiu/indexMain.js
@@ -5,4 +5,39 @@ $( "label" ).focusin(function() {
 $( "label" ).focusout(function() {
     $( this ).find( "i" ).animate({"opacity":"1"}, 200);
 });
-  
+
+$("button").click(function(){
+    $(this).toggleClass("afterSub");
+    $(this).css("transition", "all 0.5s");
+    $(this).css("width", "70px");
+    $(this).css("height", "70px");
+    $(this).text("✔");
+    $(this).css("left", "40%");
+    $(this).css("right", "");
+    $(this).css("top", "-20px");
+    $(this).css("border-radius", "50%");
+    $(this).css("background-color", "whitesmoke");
+    $(this).css("color", "#A67F78");
+    $(this).css("border", "3px #A67F78 solid");
+    // setTimeout(function(){
+    //     //do something special
+    // }, 5000);
+});
+
+$("img").hover(function(){
+    $("#tt").hide();
+}, function(){
+    $("#tt").show();
+});
+
+$(".inputButton").click(function(){
+    $("#divvv").fadeOut("slow", function(){
+        $("#regdiv").fadeIn();
+    });
+});
+
+$(".closebtn").click(function(){
+    $("#regdiv").fadeOut("slow", function(){
+        $("#divvv").fadeIn();
+    });
+});
