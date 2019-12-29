451d4f06b47c00222a865b502c7070c22b79d3ae
agimz0330 <agimz0330@gmail.com>
2019-12-29 19:18:35 +0800

    up

--- finalProject/templates_chiu/static/myPageMain.js
+++ finalProject/templates_chiu/static/myPageMain.js
@@ -3,12 +3,6 @@ function start(){
     var bookdiv= document.getElementById("divvv");
     var h= screen.clientHeight/ 2;
     bookdiv.setAttribute("style","height: "+ h+ "px;");
-
-    var smallFont= document.getElementById("fontSmall");
-    smallFont.addEventListener("click", setSmallFont, false);
-
-    var bigFont= document.getElementById("fontBig");
-    bigFont.addEventListener("click", setBigFont, false);
 }
 
 $("#menu").click(function(){
@@ -69,23 +63,20 @@ $("#smallScreen").click(function(){
     $(".adjust").removeClass("selected");
 });
 
-function setSmallFont(){
-    var bookdiv= document.getElementById("divvv");
-    var s= getComputedStyle(bookdiv).getPropertyValue("font-size");
+$("#fontSmall").click(function(){
+    var s= $("#divvv").css("font-size");
     s= s.substring(0, s.length- 2);
     s= parseInt(s);
     if(s>= 8) s-= 2;
-    bookdiv.setAttribute("style","font-size: "+ s+ "px;");
-}
-
-function setBigFont(){
-    var bookdiv= document.getElementById("divvv");
-    var s= getComputedStyle(bookdiv).getPropertyValue("font-size");
+    $("#divvv").css("font-size", s+ "px");
+});
+$("#fontBig").click(function(){
+    var s= $("#divvv").css("font-size");
     s= s.substring(0, s.length- 2);
     s= parseInt(s);
     if(s<= 50) s+= 2;
-    bookdiv.setAttribute("style","font-size: "+ s+ "px;");
-}
+    $("#divvv").css("font-size", s+ "px");
+});
 
 window.addEventListener("load", start, false);
 
